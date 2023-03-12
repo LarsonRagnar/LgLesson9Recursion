6815434dcf34ae96f190f30737e37953fdbd366a
LarsonRagnar <larionov1992@gmail.com>
2023-03-12 21:51:10 +0300

    виды операторов

---
+++ task1/Program.cs
@@ -0,0 +1,30 @@
+﻿// Унарные операторы - 1 операнд
+// Бинарные операторы -2 операнда
+
+
+// Постфиксной: "после" а++
+
+// Префиксный оператор: "перед" ++а
+
+
+// int a = 1;
+
+// int prefixIncrement = ++a * a;
+
+// Console.WriteLine(prefixIncrement); // Наибольший приоритет
+
+// int b = 1; 
+// int postfixIncrement = b++ * b; // наименьший приоритет
+// Console.WriteLine(postfixIncrement);
+
+
+// Тернарный оператор 
+
+// условие - 1 операнд ? значение для истины - 2 : значение для лжи - 3
+
+// int dreamSalary = int.MaxValue;
+
+// int realSalary = 50000;
+
+// var result = realSalary < dreamSalary ? true : false;
+
